242e07793eef15045f40a05e9946dc3921485e1b
goudan <837471654@qq.com>
2022-05-01 04:31:58 -0400

    LinkList.c successfull

--- LinearList/LinkList.c
+++ LinearList/LinkList.c
@@ -19,7 +19,7 @@ typedef struct LNode{
     struct LNode* next;
 }LNode, *PLNode;
 
-Status CreateNode_Lk(Elemtype e, PLNode newnode);
+Status CreateNode_Lk(Elemtype e, PLNode* newnode);
 
 typedef struct{
     PLNode headnode;
@@ -32,24 +32,58 @@ Status ClearList_Lk(LinkList* L);
 bool ListEmpty_Lk(LinkList* L);
 int ListLength_Lk(LinkList* L);
 Status GetElem_Lk(LinkList* L, int i, Elemtype* e);
-int LocateElem_Lk(LinkList* L, Elemtype* e, bool (*compare)(Elemtype, Elemtype));
+int LocateElem_Lk(LinkList* L, Elemtype e, bool (*compare)(Elemtype, Elemtype));
 Status ListInsert_Lk(LinkList* L, int i, Elemtype e);
 Status ListDelete_Lk(LinkList* L, int i, Elemtype* e);
+void ListPrint_Lk(LinkList* L);
 
-int main(){
-
+bool cmp(Elemtype a, Elemtype b){
+    return a==b;
 }
 
-Status CreateNode_Lk(Elemtype e, PLNode newnode){
-    newnode = (PLNode)malloc(sizeof(LNode));
-    if(newnode==NULL) return ERROR;
-    newnode->data = e;
-    newnode->next = NULL;
+int main(){
+    LinkList L;
+    printf("init\n");
+    if(ERROR == InitList_Lk(&L)) printf("ERROR\n"), exit(-1);
+    printf("insert\n");
+    for(int i = 0;i<5;i++){
+        if(ERROR == ListInsert_Lk(&L, i, i+1)) printf("ERROR\n");
+    }
+    // error 
+    if(ERROR == ListInsert_Lk(&L, 8, 0)) printf("ERROR\n");
+    printf("print list:\n");
+    ListPrint_Lk(&L);
+    printf("delete\n");
+    for(int i =0;i<3;i++){
+        int ele = 0;
+        if(ERROR == ListDelete_Lk(&L, i, &ele)) printf("ERROR\n");
+        else{
+            printf("delete elem : %d\n", ele);
+        }
+    }
+    printf("print list:\n");
+    ListPrint_Lk(&L);
+    printf("Locate:\n");
+    int i = LocateElem_Lk(&L, 4, cmp);
+    printf("Locate 2 is index %d\n", i);
+    printf("clear!\n");
+    ClearList_Lk(&L);
+    ListPrint_Lk(&L);
+    DestroyList_Lk(&L);
+    printf("Destory success\n");
+    return 0;
+}
+
+Status CreateNode_Lk(Elemtype e, PLNode* newnode){
+    PLNode nnode = (PLNode)malloc(sizeof(LNode));
+    if(nnode==NULL) return ERROR;
+    nnode->data = e;
+    nnode->next = NULL;
+    *newnode = nnode;
     return OK;
 }
 
 Status InitList_Lk(LinkList* L){
-    L = (LinkList *)malloc(sizeof(LinkList));
     if(L == NULL) return ERROR;
     L->headnode = NULL;
     L->length = 0;
@@ -103,27 +137,34 @@ Status GetElem_Lk(LinkList* L, int i, Elemtype* e){
     return OK;
 }
 
-bool cmp(Elemtype a, Elemtype b){
-    return a==b;
-}
 
-int LocateElem_Lk(LinkList* L, Elemtype* e, bool (*compare)(Elemtype, Elemtype)){
+
+int LocateElem_Lk(LinkList* L, Elemtype e, bool (*compare)(Elemtype, Elemtype)){
     if(L==NULL) return ERROR;
     int i = 0;
     PLNode pnode = L->headnode;
-    for(i = 0;pnode&&compare(pnode->data, *e);++i) pnode = pnode->next;
+    for(i = 0;pnode&&(!compare(pnode->data, e));++i) pnode = pnode->next;
     if(pnode==NULL) return ERROR;
     return i;
 }
 
 Status ListInsert_Lk(LinkList* L, int i, Elemtype e){
+    if(L == NULL) return ERROR;
+    if(i==0){
+        PLNode newnode = NULL;
+        if(ERROR == CreateNode_Lk(e, &newnode)) return ERROR;
+        newnode->next = L->headnode;
+        L->headnode = newnode;
+        ++L->length;
+        return OK;
+    }
     if(i<0||i>L->length) return ERROR;
     PLNode pnode = L->headnode;
     PLNode pnode_pre = NULL;
     while(pnode&&i) pnode_pre = pnode, pnode = pnode->next, --i;
-    if(pnode==NULL) return ERROR;
+    if(pnode_pre==NULL) return ERROR;
     PLNode newnode = NULL;
-    if(ERROR == CreateNode_Lk(e, newnode)) return ERROR;
+    if(ERROR == CreateNode_Lk(e, &newnode)) return ERROR;
     pnode_pre->next = newnode;
     newnode->next = pnode;
     ++L->length;
@@ -131,6 +172,14 @@ Status ListInsert_Lk(LinkList* L, int i, Elemtype e){
 }
 
 Status ListDelete_Lk(LinkList* L, int i, Elemtype* e){
+    if(i==0){
+        PLNode deletenode = L->headnode;
+        *e = deletenode->data;
+        L->headnode = L->headnode->next;
+        free(deletenode);
+        --L->length;
+        return OK;
+    }
     if(i<0||i>L->length) return ERROR;
     PLNode pnode = L->headnode;
     PLNode pnode_pre = NULL;
@@ -140,6 +189,19 @@ Status ListDelete_Lk(LinkList* L, int i, Elemtype* e){
     PLNode deletenode = pnode;
     pnode_pre->next = pnode->next;
     free(deletenode);
-    ++L->length;
+    --L->length;
     return OK;
+}
+
+void ListPrint_Lk(LinkList* L){
+    if(L==NULL){
+        printf("L == NULL!\n");
+        return;
+    }
+    PLNode pnode = L->headnode;
+    while(pnode){
+        printf("%d->", pnode->data);
+        pnode = pnode->next;
+    }
+    printf("\t now length: %d\n", ListLength_Lk(L));
 }
